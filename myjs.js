8fc3313ba84583409178b5e55c34d06c03f59ff4
Ryomiyamo <167832767+Ryomiyamo@users.noreply.github.com>
2024-06-25 10:45:27 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,9 +1,9 @@
 alert('Hello,Ryouhei');
 alert(1+1);
 alert(Math.sqrt(1+1));
-alert(Math.sqrt(2+3));
-alert(1/0);
-alert(0/0);
+・２の平方根と3の平方根の和を計算しなさい。
+・1を0でわるとどうなるか、試してみなさい。
+・0を0で割るとどうなるか、試してみなさい。
 alert(2+3);
 alert('2'+'3');
 alert(Number('2')+Number('3'));
